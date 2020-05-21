ec6a3c1b3f5d5eeb76c6f52edb8cb818590d9122
Matyas Orhidi <morhidi@cloudera.com>
2020-05-21 14:50:04 +0200

    fixing KeyGenerator

--- src/main/java/com/cloudera/streaming/examples/flink/operators/KeyGenerator.java
+++ src/main/java/com/cloudera/streaming/examples/flink/operators/KeyGenerator.java
@@ -3,19 +3,18 @@ package com.cloudera.streaming.examples.flink.operators;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 
-
 import java.util.Random;
 
 public class KeyGenerator implements KeySelector<String, Integer> {
 
-    private final int numKeys;
+	private final int numKeys;
 
-    public KeyGenerator(int numKeys) {
-        this.numKeys = numKeys;
-    }
+	public KeyGenerator(int numKeys) {
+		this.numKeys = numKeys;
+	}
 
-    @Override
-    public Integer getKey(String message) throws Exception {
-        return message.hashCode() % numKeys;
-    }
+	@Override
+	public Integer getKey(String message) throws Exception {
+		return Math.abs(message.hashCode()) % numKeys;
+	}
 }
