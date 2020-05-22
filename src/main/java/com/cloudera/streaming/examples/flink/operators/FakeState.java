c14a240e140c55f7b6ab0a49d5e34bf8aa4a9385
Matyas Orhidi <morhidi@cloudera.com>
2020-05-22 15:51:09 +0200

    fixing fakestate

--- src/main/java/com/cloudera/streaming/examples/flink/operators/FakeState.java
+++ src/main/java/com/cloudera/streaming/examples/flink/operators/FakeState.java
@@ -6,16 +6,14 @@ import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.util.StringUtils;
 
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FakeState extends RichMapFunction<String, String> {
 	private transient ValueState<String> valueState;
-
-	private final String fakeState;
+	private final int size;
 
 	public FakeState(int size) {
-		fakeState = StringUtils.generateRandomAlphanumericString(ThreadLocalRandom.current(), size);
+		this.size = size;
 	}
 
 	@Override
@@ -28,7 +26,7 @@ public class FakeState extends RichMapFunction<String, String> {
 	public String map(String s) throws Exception {
 		String value = valueState.value();
 		if (value == null) {
-			valueState.update(fakeState);
+			valueState.update(StringUtils.generateRandomAlphanumericString(ThreadLocalRandom.current(), size));
 		} else {
 			valueState.update(value);
 		}
